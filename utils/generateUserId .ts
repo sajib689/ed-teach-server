50063e071c0c11232e46ef663952016e2f7a65a3
sajib689 <jibonraja11@gmail.com>
2025-01-10 11:30:17 +0600

    add function to find last user ID in the database

--- utils/generateUserId .ts
+++ utils/generateUserId .ts
@@ -1,5 +1,9 @@
 
+export const findLastUserId = async () => {
+    // Find the last user in the database
+    // Return the id of the last user
+}
 
 export const generateUserId =async () => {
-    
+
 }
