128827cc2aa67d1fced7875a18630fd62856a5a7
sajib689 <jibonraja11@gmail.com>
2025-01-11 13:13:46 +0600

    update sendUsers function to return a success message along with user data

--- controllers/users.controllers.ts
+++ controllers/users.controllers.ts
@@ -2,7 +2,10 @@ import { Request, Response } from "express";
 import { createUsers } from "../services/users.services";
 
 const sendUsers = async (res: Response, req: Request) => {
-    const {user} = await req.body 
-    const result = await createUsers(user)
-    res.status(200).json({data: result})
-}
+  const { user } = await req.body;
+  const result = await createUsers(user);
+  res.status(200).json(
+    {   message: "User created successfully",
+        data: result }
+);
+};
