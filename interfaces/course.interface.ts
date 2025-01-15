a6dd30cccbde6fd76fbd87ef0e1242d09e0d25bb
sajib689 <jibonraja11@gmail.com>
2025-01-15 21:24:57 +0600

    feat: add Course interface to define course properties and structure

---
+++ interfaces/course.interface.ts
@@ -0,0 +1,13 @@
+export interface Course {
+    id: number; // Unique identifier for the course
+    courseImage: string; // URL of the course image
+    courseName: string; // Name of the course
+    providerName: string; // Name of the course provider
+    providerImage: string; // URL of the provider's image
+    providerTitle: string; // Title/position of the provider
+    price: string; // Price of the course (as a string for display purposes)
+    totalEnrolls?: string; // Total number of enrollments (optional)
+    courseCategory: string; // Category of the course (e.g., Frontend Development, Backend Development)
+    courseType: string; // Type of course (e.g., "featured course", "all course")
+  }
+  
