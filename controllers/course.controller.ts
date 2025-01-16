c40f9279eebf1a0e9166da2f33a4019868eb6c13
sajib689 <jibonraja11@gmail.com>
2025-01-16 17:46:27 +0600

    feat: add getCourseByIdController to handle fetching a course by ID

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { courseService, getCourseService } from "../services/course.service";
+import { courseService, getCourseByIdService, getCourseService } from "../services/course.service";
 
 export const courseController = async (req: Request,res: Response) => {
     try {
@@ -19,6 +19,19 @@ export const getCourseController = async ( req: Request,res: Response) => {
         const result = await getCourseService();
         return res.status(200).json({
             message: "Courses fetched successfully",
+            data: result
+        })
+    } catch (err: any) {
+        return res.status(500).json({err: err.message})
+    }
+}
+
+export const getCourseByIdController = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const result = await getCourseByIdService(id);
+        return res.status(200).json({
+            message: "Course fetched successfully",
             data: result
         })
     } catch (err: any) {
