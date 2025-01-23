ccd5fb22c5d067a0d41b06b30a6e71b8482cf44e
sajib689 <jibonraja11@gmail.com>
2025-01-23 13:07:32 +0600

    feat: implement Socket.io for real-time communication and update server initialization

--- server.ts
+++ server.ts
@@ -37,11 +37,22 @@ const connectDB = async () => {
 
 // Socket.io connection
 const server = createServer(app);
-
-// Start the server
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  }
+})
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  })
+})
+// Start the server with the specified port
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () => {
+server.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   await connectDB();
 });
