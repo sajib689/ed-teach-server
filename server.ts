102e18b96e3d6fca375d0f2b926f53d82c66226b
sajib689 <jibonraja11@gmail.com>
2025-01-23 20:52:19 +0600

    feat: add PaymentData interface for payment processing

--- server.ts
+++ server.ts
@@ -7,6 +7,7 @@ import courseRouter from './routes/course.route';
 import { createServer } from "http";
 import { Server } from "socket.io";
 import SSLCommerzPayment from 'sslcommerz-lts';
+import { PaymentData } from './interfaces/payment.interface';
 
 // Load environment variables
 dotenv.config();
@@ -62,25 +63,6 @@ io.on('connection', (socket) => {
     io.emit("activeUsers", activeUsers);
   });
 });
-interface PaymentData {
-  total_amount: number;
-  currency: string;
-  tran_id: string;
-  success_url: string;
-  fail_url: string;
-  cancel_url: string;
-  ipn_url: string;
-  product_name: string;
-  product_category: string;
-  product_profile: string;
-  cus_name: string;
-  cus_email: string;
-  cus_phone: string;
-  cus_add1: string;
-  cus_city: string;
-  cus_country: string;
-  shipping_method?: string; 
-}
 
 
 // Payment gateway initialization
