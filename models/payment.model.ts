dd9a276e9f433b88d7c3c0e6f63589c9080454af
sajib689 <jibonraja11@gmail.com>
2025-01-24 13:15:35 +0600

    feat: implement payment service for creating payments and update payment schema

--- models/payment.model.ts
+++ models/payment.model.ts
@@ -2,7 +2,8 @@ import { model, Model, Schema } from "mongoose";
 import { IPayment } from "../interfaces/paymentSuccess.interface";
 
 type paymentModel = Model<IPayment, object>;
-export const paymentSchema = new Schema<IPayment>({
+export const paymentSchema = new Schema<IPayment>(
+  {
     tran_id: { type: String, required: true, unique: true },
     amount: { type: Number, required: true },
     status: { type: String, required: true },
@@ -16,6 +17,7 @@ export const paymentSchema = new Schema<IPayment>({
     },
     courseName: { type: String, required: true },
   },
-  { timestamps: true })
+  { timestamps: true }
+);
 
-  export const Payment = model<IPayment, paymentModel>("Payment", paymentSchema);
+export const Payment = model<IPayment, paymentModel>("Payment", paymentSchema);
