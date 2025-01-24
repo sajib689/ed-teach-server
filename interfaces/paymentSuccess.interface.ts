f410171f699b84e3e5f1228eb3bcbb6fefdcd030
sajib689 <jibonraja11@gmail.com>
2025-01-24 21:48:00 +0600

    feat: enhance payment initialization and success handling; consolidate customer data and improve validation

--- interfaces/paymentSuccess.interface.ts
+++ interfaces/paymentSuccess.interface.ts
@@ -1,15 +1,13 @@
 export interface IPayment {
-    tran_id: string;
-    amount: number;
-    status: string;
-    customer: {
-      name: string;
-      email: string;
-      phone: string;
-      address?: string;
-      city?: string;
-      country?: string;
-    };
-    courseName: string;
-    createdAt: Date;
-  }
+  tran_id: string;
+  courseName: string;
+  customer: {
+    name: string;
+    email: string;
+    phone: string;
+  }; // Consolidate customer data
+  total_amount: number;
+  status: string; // Example: "PENDING", "SUCCESS", "FAILED"
+  createdAt?: Date;
+  updatedAt?: Date;
+}
