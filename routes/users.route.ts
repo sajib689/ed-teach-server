f68af5e633cf37ef7ddb0750cca6ac960284ff67
sajib689 <jibonraja11@gmail.com>
2025-02-15 19:35:05 +0600

    feat: add route to retrieve admin user by role

--- routes/users.route.ts
+++ routes/users.route.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { getUsersByEmailController, sendUsers } from "../controllers/users.controllers";
+import { getAdminController, getUsersByEmailController, sendUsers } from "../controllers/users.controllers";
 import { asyncHandler } from '../middlewares/asyncHandler';
 
 const router = express.Router();
 
 router.post('/users',asyncHandler(sendUsers))
 router.get('/users/:email',asyncHandler(getUsersByEmailController))
-
+router.get('/users/:role',asyncHandler(getAdminController))
 
 export default router;
