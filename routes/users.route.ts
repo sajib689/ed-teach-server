81fde5569751ffe2a36aae7e771a8b3a8cf3cad2
sajib689 <jibonraja11@gmail.com>
2025-02-19 20:36:00 +0600

    addd some comments

--- routes/users.route.ts
+++ routes/users.route.ts
@@ -3,9 +3,12 @@ import { getAdminController, getUsersByEmailController, getUsersController, send
 import { asyncHandler } from '../middlewares/asyncHandler';
 
 const router = express.Router();
-
+// post the users data to db
 router.post('/users',asyncHandler(sendUsers))
+// get the users by email
 router.get('/users/:email',asyncHandler(getUsersByEmailController))
+// get the all users
 router.get('/users',asyncHandler(getAdminController))
+// get the admin
 router.get('/userss',asyncHandler(getUsersController))
 export default router;
